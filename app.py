c4e3c541f10b5b109150a0f2becd501467ef4742
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-05 19:39:46 +0530

    Configure login view and update app run settings
    Set the login view for the login manager and adjust app.run()
    
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -13,6 +12,14 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = "auth.home"
+
+    # IMPORTANT: import models AFTER db init
+    from models.models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from routes.auth import auth_bp
     from routes.cart import cart_bp
@@ -22,9 +29,12 @@ def create_app():
     app.register_blueprint(cart_bp)
     app.register_blueprint(admin_bp)
 
+    with app.app_context():
+        db.create_all()
+
     return app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
