b52bdbd566cd54210e808a6b8ae369f13356e822
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-05 20:57:37 +0530

    Update app.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask
 from flask_login import LoginManager
 #from config import Config
 from db import db, init_db
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://dariya:iXXzwPV9FQzQLqEUw35PJq9f3ItUT4rX@dpg-d5j4o50gjchc73cu2hsg-a/shop_aizq"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
 
 
 def create_app():
     app = Flask(__name__)
-
+    
     # Load config
     app.config["SECRET_KEY"] = "secret"
 
