5b00b39cee072eb7bce27941d02c43d46cd30979
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-05 21:00:58 +0530

    Update app.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 #from config import Config
-from db import db, init_db
+#from db import db, init_db
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://dariya:iXXzwPV9FQzQLqEUw35PJq9f3ItUT4rX@dpg-d5j4o50gjchc73cu2hsg-a/shop_aizq"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
