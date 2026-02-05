43aee9bf93469bbb40a6d43d0a8f348580a60f60
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-05 20:12:23 +0530

    Update config.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
-
 import os
 
 class Config:
-    SECRET_KEY = "secretkey"
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "sqlite:///shop.db"
-    )
+    SECRET_KEY = "supersecretkey"
+
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    # Render postgres fix
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace(
+            "postgres://", "postgresql://", 1
+        )
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
