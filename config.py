4a4709f55c8a1d66eadd98220c6c6e731503e745
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-05 20:33:27 +0530

    Update config.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- config.py
+++ config.py
@@ -1,15 +1,6 @@
 import os
 
-class Config:
-    SECRET_KEY = "supersecretkey"
-
-    DATABASE_URL = os.getenv("DATABASE_URL")
-
-    # Render postgres fix
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace(
-            "postgres://", "postgresql://", 1
-        )
-
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://dariya:iXXzwPV9FQzQLqEUw35PJq9f3ItUT4rX@dpg-d5j4o50gjchc73cu2hsg-a/shop_aizq"
+)
