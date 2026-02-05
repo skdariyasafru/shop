18bef5130855967993d22cd74d3f3ed300c8e9e3
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-05 20:41:37 +0530

    Update config.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://dariya:iXXzwPV9FQzQLqEUw35PJq9f3ItUT4rX@dpg-d5j4o50gjchc73cu2hsg-a/shop_aizq"
-)
+class Config:
+    SECRET_KEY = "supersecretkey"
+
+    DATABASE_URL = os.getenv(
+        "DATABASE_URL",
+        "postgresql://dariya:iXXzwPV9FQzQLqEUw35PJq9f3ItUT4rX@dpg-d5j4o50gjchc73cu2hsg-a/shop_aizq"
+    )
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
