dde46d78fb4cdd593f31cc74b403903ebc476c1b
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-06 07:07:47 +0530

    Update models.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- models/models.py
+++ models/models.py
@@ -1,13 +1,7 @@
-
 from db import db
 from flask_login import UserMixin
 
-class User(db.Model, UserMixin):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(200))
-
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    price = db.Column(db.Float)
