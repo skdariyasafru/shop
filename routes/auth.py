47dea2a3f5406578093ea291d8e6edce2c66b047
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-06 07:51:31 +0530

    Update auth.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- routes/auth.py
+++ routes/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from models.models import User
-from ap import db
+from db import db
 
 auth_bp = Blueprint("auth", __name__)
 
