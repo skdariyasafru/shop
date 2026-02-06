c250e0e882d690c21c7abf1a83097c154e233257
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-06 08:03:41 +0530

    Update db_init.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- db_init.py
+++ db_init.py
@@ -2,6 +2,9 @@ from db import db
 
 def create_tables(app):
     with app.app_context():
-        from models.models import User  # import all models here
+        from models.models import User  # import all models
+
+        # Create tables only if they don't exist
         db.create_all()
-        print("✅ Tables created successfully!")
+
+        print("✅ Tables created if not exist (data safe)")
