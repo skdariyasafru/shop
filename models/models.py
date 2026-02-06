fb03888103c56a44f8196aa25944ec628dfc93c3
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-06 20:53:44 +0530

    Update models.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- models/models.py
+++ models/models.py
@@ -2,16 +2,16 @@ from db import db
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
-id = db.Column(db.Integer, primary_key=True)
-username = db.Column(db.String(100), unique=True)
-password = db.Column(db.String(200))
+     id = db.Column(db.Integer, primary_key=True)
+     username = db.Column(db.String(100), unique=True)
+     password = db.Column(db.String(200))
 
 class Product(db.Model):
-id = db.Column(db.Integer, primary_key=True)
-name = db.Column(db.String(200))
-price = db.Column(db.Float)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    price = db.Column(db.Float)
 
 class Cart(db.Model):
-id = db.Column(db.Integer, primary_key=True)
-user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-product_id = db.Column(db.Integer, db.ForeignKey("product.id"))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"))
