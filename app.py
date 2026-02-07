a4efea7449cc8de07f46019f99d8fb6fc62188fb
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-07 08:11:04 +0530

    Update app.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- app.py
+++ app.py
@@ -23,7 +23,9 @@ def create_app():
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-
+    @app.route("/")
+    def home():
+        return render_template("index.html")
     # =====================
     # CART ROUTES
     # =====================
