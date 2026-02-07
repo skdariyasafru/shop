a253413d7d257e9ea8d8c9376f7dd9b1811b2c0e
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-07 21:39:45 +0530

    Update models.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- models/models.py
+++ models/models.py
@@ -28,6 +28,8 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     price = db.Column(db.Float)
+    image = db.Column(db.String(300))
+
 
 
 
