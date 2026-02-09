75918bc234ed3407b22f82d580370b29b0eab5e5
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-09 07:47:51 +0530

    Update models.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- models/models.py
+++ models/models.py
@@ -1,14 +1,15 @@
 from db import db
 from flask_login import UserMixin
 
-class Cart(db.Model):
-    __tablename__ = "cart"
 
+
+class Cart(db.Model):
+     __tablename__ = "cart"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
+    user_id = db.Column(db.Integer)
     product_name = db.Column(db.String(200))
     price = db.Column(db.Float)
-
+    quantity = db.Column(db.Integer, default=1)
 
 class Order(db.Model):
     __tablename__ = "order"
