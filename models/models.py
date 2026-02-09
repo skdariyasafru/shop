8c18798949658ad041e59fdbc05ef3751fcfc76c
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-09 19:20:07 +0530

    Update models.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- models/models.py
+++ models/models.py
@@ -2,23 +2,6 @@ from db import db
 from flask_login import UserMixin
 
 
-
-class Cart(db.Model):
-     __tablename__ = "cart"
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    product_name = db.Column(db.String(200))
-    price = db.Column(db.Float)
-    quantity = db.Column(db.Integer, default=1)
-
-class Order(db.Model):
-    __tablename__ = "order"
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
-    product_name = db.Column(db.String(200))
-    price = db.Column(db.Float)
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
@@ -32,5 +15,18 @@ class Product(db.Model):
     image = db.Column(db.String(300))
 
 
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    product_name = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
 
 
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    product_name = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+    total = db.Column(db.Float)
