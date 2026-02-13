6396a2bce8a1b4a8aad98e62deec38668ac0de69
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-13 19:56:01 +0530

    Update db.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- db.py
+++ db.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Shaik09871234@db.ujlsspolbtusivmilioh.supabase.co:5432/postgres?sslmode=require
-"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Shaik09871234@db.ujlsspolbtusivmilioh.supabase.co:5432/postgres?sslmode=require"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
