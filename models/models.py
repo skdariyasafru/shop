76c963c46ffabbc2514840aed99b2ee6c66362b7
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-15 12:38:16 +0530

    Add files via upload
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- models/models.py
+++ models/models.py
@@ -1,27 +1,22 @@
+
 from db import db
 from flask_login import UserMixin
 
-
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(200))
-
+    password = db.Column(db.String(100))
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     price = db.Column(db.Float)
-    image = db.Column(db.String(300))
-
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     product_id = db.Column(db.Integer)
-    quantity = db.Column(db.Integer, default=1)
-
-
+    quantity = db.Column(db.Integer)
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
