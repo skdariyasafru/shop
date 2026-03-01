1b393020b49f8a7291406b1f0bc58bdaf7ce898e
skdariyasafru <sk.dariyasafru@gmail.com>
2026-03-01 16:30:18 +0530

    Update app.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- app.py
+++ app.py
@@ -310,3 +310,7 @@ def create_app():
 
 # 🔥 IMPORTANT FOR GUNICORN
 app = create_app()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
