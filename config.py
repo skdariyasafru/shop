4a42bf77d39a3c0c09ab833287a288d93943bc77
skdariyasafru <sk.dariyasafru@gmail.com>
2026-03-01 20:49:22 +0530

    Update config.py
    Signed-off-by: skdariyasafru <sk.dariyasafru@gmail.com>

--- config.py
+++ config.py
@@ -13,9 +13,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_size": 5,
-        "max_overflow": 10,
-        "pool_timeout": 30,
-        "pool_recycle": 1800,
-    }
+    
